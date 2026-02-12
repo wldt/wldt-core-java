ec8fecd05998ed4286e48878cbf30abfedccd855
Marco Picone <picone.m@gmail.com>
2026-02-12 10:57:34 +0100

    Initial refactoring and implementation of core Augmentation classes

---
+++ src/main/java/it/wldt/augmentation/AugmentationFunctionDescription.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2025 - Current Year
+ * Marco Picone Ph.D
+ * Email: picone.m@gmail.com
+ * Website: https://www.marcopicone.net/
+ * All rights reserved.
+ *
+ * This program is provided under a Dual Licensing model:
+ * 1) GNU General Public License version 3.0 (GPL-3.0) for open-source, academic,
+ *    research, non-profit, and other non-commercial use; or
+ * 2) Commercial License, for any commercial use, proprietary development, or
+ *    closed-source distribution. To obtain a Commercial License, please contact: Marco Picone (picone.m@gmail.com)
+ *
+ * By using this software, you agree to comply with the terms of the applicable license.
+ * This applies to all forms of the software, including source code and compiled/binary forms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+package it.wldt.augmentation;
+
+public class AugmentationFunctionDescription {
+
+    /**
+     * The unique id of the augmentation function.
+     */
+    private String id;
+
+    /**
+     * The name of the augmentation function.
+     */
+    private String name;
+
+    /**
+     * The description of the augmentation function.
+     */
+    private String description;
+
+    /**
+     * The version of the augmentation function.
+     */
+    private String version;
+
+    /**
+     * The type of the augmentation function.
+     */
+    private AugmentationFunctionType type;
+
+    /**
+     * The context request of the augmentation function.
+     */
+    private AugmentationFunctionContextRequest contextRequest;
+
+
+
+}
